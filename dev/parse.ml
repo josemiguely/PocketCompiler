0e7880b84e395658eb774d22ddbd94dd59bd776b
Gedoix <diegoortegop@hotmail.com>
2021-09-27 09:51:46 -0300

    Parser for 1st order and foreign function declarations

--- dev/parse.ml
+++ dev/parse.ml
@@ -9,11 +9,16 @@ let rec parse_exp (sexp : sexp) : expr =
   | `Atom "false" -> Bool false
   | `Atom s -> (
     match Int64.of_string_opt s with Some n -> Num n | None -> Id s )
+  | `List (`Atom "@sys" :: `Atom name :: args) -> 
+    match args with
+    | [] -> failwith (sprintf "Invalid foreign function call: %s" (to_string sexp))
+    | _ -> ApplyFF (name, List.map parse_exp args)
   | `List [eop; e] -> (
     match eop with 
     | `Atom "add1" -> Prim1 (Add1, parse_exp e)
     | `Atom "sub1" -> Prim1 (Sub1, parse_exp e)
-    | _ -> failwith (sprintf "Not a valid expr: %s" (to_string sexp)) )
+    | _ -> Apply (parse_exp eop, [parse_exp e])
+    )
   | `List [eop; e1; e2] -> (
     match eop with
     | `Atom "let" -> (
@@ -23,9 +28,44 @@ let rec parse_exp (sexp : sexp) : expr =
     | `Atom "+" -> Prim2 (Add, parse_exp e1, parse_exp e2)
     | `Atom "and" -> Prim2 (And, parse_exp e1, parse_exp e2)
     | `Atom "<=" -> Prim2 (Lte, parse_exp e1, parse_exp e2)
-    | _ -> failwith (sprintf "Not a valid expr: %s" (to_string sexp)) )
+    | _ -> Apply (parse_exp eop, [parse_exp e1 ; parse_exp e2])
+    )
   | `List [`Atom "if"; e1; e2; e3] -> If (parse_exp e1, parse_exp e2, parse_exp e3)
+  | `List (e1 :: e2) -> Apply (parse_exp e1, List.map parse_exp e2)
   | _ -> failwith (sprintf "Not a valid expr: %s" (to_string sexp))
+
+let rec parse_prog (sexp : sexp) : prog =
+  match sexp with
+  | `List (hd :: tl) -> (
+    match hd with
+    | `List [`Atom "def" ; `List (`Atom name :: args) ; body] ->
+      let (fundefs, expr) = parse_prog (`List tl) in
+      let arg_names = List.map parse_arg_name args in
+      [ Native (name, arg_names, parse_exp body) ] @ fundefs, expr
+    | `List (`Atom "defsys" :: `Atom name :: arg_spec) -> (
+      match List.rev arg_spec with
+      | (ret :: `Atom "->" :: args) -> 
+        let (fundefs, expr) = parse_prog (`List tl) in
+        let arg_types = List.map parse_c_type (List.rev args) in
+        let ret_type = parse_c_type ret in
+        [ Foreign (name, arg_types, ret_type) ] @ fundefs, expr
+      | _ -> failwith (sprintf "Not a valid type declaration: %s" (to_string (`List arg_spec)))
+    )
+    | _ -> [], parse_exp sexp
+  )
+  | _ -> [], parse_exp sexp
+
+and parse_arg_name (sexp : sexp) : string =
+  match sexp with
+  | `Atom name -> name
+  | _ -> failwith (sprintf "Not a valid argument name: %s" (to_string sexp))
+
+and parse_c_type (sexp : sexp) : ctype =
+  match sexp with
+  | `Atom "any" -> CAny
+  | `Atom "int" -> CInt
+  | `Atom "bool" -> CBool
+  | _ -> failwith (sprintf "Not a valid c type: %s" (to_string sexp))
 
 let sexp_from_file : string -> CCSexp.sexp =
  fun filename ->
