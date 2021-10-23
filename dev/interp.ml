dd676e23ec7d241bdf501057b67ac1d4f01d422b
Gedoix <diegoortegop@hotmail.com>
2021-10-23 01:27:59 -0300

    Closure creation

--- dev/interp.ml
+++ dev/interp.ml
@@ -156,6 +156,7 @@ let rec interp expr env fenv =
           let t = (interp e env fenv) in
           let i = (interp k env fenv) in
           set_elem t i (interp v env fenv)
+  | Lambda (params, body) -> ClosureV (env, params, body)
 
 let interp_prog prog env =
   let defs, expr = prog in
